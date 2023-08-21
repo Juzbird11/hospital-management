447d4cc781fb620e4575283b739af4004a69b79f
Juz Bird <juzbird11@gmail.com>
2023-08-21 23:26:06 +0700

    pass Booking Token Management

---
+++ app/Http/Controllers/OutPatientBookingController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OutPatientBooking;
+use Illuminate\Http\Request;
+
+class OutPatientBookingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OutPatientBooking $outPatientBooking)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OutPatientBooking $outPatientBooking)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OutPatientBooking $outPatientBooking)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/OutPatientBookingTokenController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OutPatientBookingToken;
+use Illuminate\Http\Request;
+
+class OutPatientBookingTokenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OutPatientBookingToken $outPatientBookingToken)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OutPatientBookingToken $outPatientBookingToken)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OutPatientBookingToken $outPatientBookingToken)
+    {
+        //
+    }
+}
